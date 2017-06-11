a830d76509c1b4b40e761f4797d6ab35cb44e789
Lisa Huang <lisa.yc.huang@gmail.com>
2017-06-11 17:56:39 +0100

    Updated cli.rb, refactoring code into #select_theme, #list_destinations and #select_country

--- lib/travel_inspiration/cli.rb
+++ lib/travel_inspiration/cli.rb
@@ -1,11 +1,13 @@
+require 'pry'
 require_relative 'themes.rb'
 require_relative 'destinations.rb'
 
 module TravelInspiration
     class CLI
-        def call
+        def start
             list_themes
-            menu
+            select_theme
+            select_country
             goodbye
         end
 
@@ -16,27 +18,40 @@ module TravelInspiration
             puts "----------------------" #horizontal divider
         end
 
-        def menu
+        def select_theme
             input = nil
             while input != "exit"
-                puts "Please select a theme from 1 - 12"
                 input = gets.strip.to_i
 
                 if input > 0 && input < 13
                     puts "----------------------" #horizontal divider
-
                     theme = TravelInspiration::Theme.list_theme_names[input-1]
-                    puts "Here are the top 6 destinations for #{theme}"
-                    puts TravelInspiration::Destination.list_destination_names(input-1)
+                    puts "Here are the top 6 destinations for #{theme.upcase}"
+                    list_destinations(input)
+                else
+                    puts "That selection is not valid. Please select a theme from 1 - 12, or type exit."
+                end
+            end
+        end
 
+        def list_destinations(input)
+            puts "Please select a destination from 1 - 6"
+            puts TravelInspiration::Destination.list_destination_names(input-1)
+            puts "----------------------" #horizontal divider
+        end
+
+        def select_country
+            input = nil
+            while input != "exit"
+                input = gets.strip.to_i
+
+                if input > 0 && input < 6
                     puts "Which destination will you like to explore? Enter 1 - 6"
                     input = gets.strip.to_i
                 else
                     puts "That selection is not valid. Try again, or type exit."
                 end
-                
             end
-
         end
 
         def goodbye
@@ -45,4 +60,4 @@ module TravelInspiration
     end
 end
 
-puts TravelInspiration::CLI.new.call
+puts TravelInspiration::CLI.new.start
